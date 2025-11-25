5cd3c96926109c82f2b92892f9c8e669eb35d310
Alan Santos <alanssantos32@gmail.com>
2025-11-25 08:44:43 -0300

    Add files via upload

---
+++ tarefa4/soma8b.vhd
@@ -0,0 +1,22 @@
+Library IEEE;
+USE IEEE.std_logic_1164.all;
+USE IEEE.std_logic_unsigned.all;
+
+Entity soma8b is
+Port(a,b : in std_logic_vector(7 downto 0);
+     s : out std_logic_vector(7 downto 0);
+	  cout : out std_logic
+);
+End soma8b;
+
+architecture arq of soma8b is
+signal aux_a,aux_b,aux_s : std_logic_vector(8 downto 0);
+begin
+
+aux_a <= '0'& a;
+aux_b <= '0'& b;
+aux_s <= aux_a + aux_b;--somador de 9 bits
+s <= aux_s(7 downto 0);-- soma de 8 bits
+cout <= aux_s(8);--carry out
+
+end arq;
